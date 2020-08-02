e6b29c968e134a5b2ac067e17ff5d86bcb1cc857
Avinash Kumar <avinashrkmv@gmail.com>
2020-08-02 12:18:17 +0530

    inspection page added

---
+++ FRONTEND/Inspection-page/js/front.js
@@ -0,0 +1,46 @@
+$(function () {
+
+    utils();
+
+});
+
+function utils() {
+
+    /* tooltips */
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /* external links in new window*/
+
+    $('.external').on('click', function (e) {
+
+        e.preventDefault();
+        window.open($(this).attr("href"));
+    });
+    /* animated scrolling */
+
+    $('.scroll-to, .scroll-to-top').click(function (event) {
+
+        var full_url = this.href;
+        var parts = full_url.split("#");
+        if (parts.length > 1) {
+
+            scrollTo(full_url);
+            event.preventDefault();
+        }
+    });
+
+    function scrollTo(full_url) {
+        var parts = full_url.split("#");
+        var trgt = parts[1];
+        var target_offset = $("#" + trgt).offset();
+        var target_top = target_offset.top - 100;
+        if (target_top < 0) {
+            target_top = 0;
+        }
+
+        $('html, body').animate({
+            scrollTop: target_top
+        }, 1000);
+    }
+}
